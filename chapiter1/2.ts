000be521aa5f3b8a44315c11debf933b67e54a73
Erivan Couttolenc 2024 <proerivan@gmail.com>
2024-09-01 12:39:47 +0200

    Update 2.ts

--- chapiter1/2.ts
+++ chapiter1/2.ts
@@ -3,7 +3,6 @@
 If we open the browser, you will see that it adds both values, but the value is incorrect. 
 This happens because it adds strings and not numbers.*/
 
-
 /*
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1");
@@ -12,33 +11,21 @@ const input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
-
-
-
-
-
-
-
-
 /*
+
 button.addEventListener("Click", function() {
     console.log(add(input1.value, input2.value));
 });
 */
 
-
-
-
 /*The solution is to use types:*/
+
 const button = document.querySelector("button")!;
 const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
-
-
 function add(num1: number, num2: number): number {
     return num1 + num2;
 }
-
 button.addEventListener("click", function() {
     const num1 = parseFloat(input1.value);
     const num2 = parseFloat(input2.value);
